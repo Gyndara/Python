d50ad8b766a47b9cbcbdf938fcdb2c2e41be7f11
Bisma <111258104+Gyndara@users.noreply.github.com>
2024-10-31 19:58:01 +0700

    Add files via upload

---
+++ LuasSegitiga.py
@@ -0,0 +1,20 @@
+a= input('masukan alas:')
+b= input('masukan tinggi:')
+c= float(a)
+d= float(b)
+
+e = 0.5 * c * d
+
+f= print('luas segitiga:', e)
+print('---------------------')
+g= input('masukan jari-jari:')
+h= float(g)
+
+i= 3.14 * h * h
+j= 2 * 3.14 * h
+
+hasil_bulat1= round(i)
+hasil_bulat2= round(j)
+
+print('luas lingkaran:', hasil_bulat1)
+print('keliling lingkaran:', hasil_bulat2)
