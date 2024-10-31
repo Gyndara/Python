ffc238944a5810d72d0b377ec8e123aca0496427
Bisma <111258104+Gyndara@users.noreply.github.com>
2024-10-31 19:57:33 +0700

    Add files via upload

---
+++ konversiWaktu2.py
@@ -0,0 +1,14 @@
+
+NilaiDetik = int(input('Masukan Nilai Detik'))
+
+hari = NilaiDetik // (24 * 3600)
+NilaiDetik = NilaiDetik % (24 * 3600)
+jam = NilaiDetik // 3600
+NilaiDetik = NilaiDetik % 3600
+menit = NilaiDetik // 60
+detik = NilaiDetik % 60
+
+print('Jumlah :', hari, ' hari')
+print('Jumlah :', jam, ' jam')
+print('Jumlah :', menit, ' menit')
+print('Jumlah :', detik, ' detik')
